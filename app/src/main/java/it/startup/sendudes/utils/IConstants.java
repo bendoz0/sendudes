09f324d4057201756debfc9afecfbdfee2caa437
rohit <verma.rohit.1203@gmail.com>
2024-11-17 17:44:51 +0100

    Added send file button and constants

---
+++ app/src/main/java/it/startup/sendudes/utils/IConstants.java
@@ -0,0 +1,10 @@
+package it.startup.sendudes.utils;
+
+public interface IConstants {
+    int PING_PORT = 8000;
+    int RECEIVE_PORT = 8001;
+    int FILE_TRANSFER_PORT = 8002;
+    String MSG_CLIENT_NOT_RECEIVING = "NOT_RECEIVING";
+    String MSG_CLIENT_PING = "PING";
+    String MSG_CLIENT_RECEIVING = "RECEIVING";
+}
