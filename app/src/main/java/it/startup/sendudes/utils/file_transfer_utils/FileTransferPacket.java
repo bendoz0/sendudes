db64c4699ddbed9190d81fbce79d0c6ba3dc441b
RobStevenGerardGueva <steven.guevarra.1@gmail.com>
2024-12-03 10:08:37 +0100

    Added getters and Gson functions

--- app/src/main/java/it/startup/sendudes/utils/file_transfer_utils/FileTransferPacket.java
+++ app/src/main/java/it/startup/sendudes/utils/file_transfer_utils/FileTransferPacket.java
@@ -1,21 +1,41 @@
 package it.startup.sendudes.utils.file_transfer_utils;
 
+import com.google.gson.Gson;
+
 public class FileTransferPacket {
     private String userName;
     private String fileName;
     private long fileSize;
-
+    private static Gson gson = new Gson();
     public FileTransferPacket(String userName, String fileName, long fileSize) {
         this.userName = userName;
         this.fileName = fileName;
         this.fileSize = fileSize;
     }
 
-    public String toJson(){
-        return "{"+
-                "userName: " + this.userName + ", " +
-                "fileName: " + this.fileName + ", " +
-                "fileSize: " + this.fileSize +
-                "}";
+    public static String toJson(FileTransferPacket ft){
+//        return "{"+
+//                "userName: " + this.userName + ", " +
+//                "fileName: " + this.fileName + ", " +
+//                "fileSize: " + this.fileSize +
+//                "}";
+        return gson.toJson(ft);
+    }
+
+    public static FileTransferPacket fromJson(String str){
+        return gson.fromJson(str, FileTransferPacket.class);
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+    public String getFileName(){
+        return fileName;
+    }
+    public long getFileSize(){
+        return fileSize;
+    }
+    public String toString(){
+        return "Username: " + this.userName + "\nFile: " + this.fileName + "\nSize: " + fileSize + " Bytes";
     }
 }
