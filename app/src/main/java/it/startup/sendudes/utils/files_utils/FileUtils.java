567262df5ccf8625779549742b78a102e55d3666
rohit <verma.rohit.1203@gmail.com>
2024-12-13 23:43:48 +0100

    Refactored a method

--- app/src/main/java/it/startup/sendudes/utils/files_utils/FileUtils.java
+++ app/src/main/java/it/startup/sendudes/utils/files_utils/FileUtils.java
@@ -71,41 +71,47 @@ public class FileUtils {
 
         if (!selectedFile.equalsIgnoreCase("Select a file")) {
             try {
-                Size mSize = new Size(width, height);
-                CancellationSignal ca = new CancellationSignal();
-                Bitmap bitmapThumbnail = activity.getContentResolver().loadThumbnail(selectedFileUri, mSize, ca);
-                binding.selectedFileThumbnail.setImageBitmap(bitmapThumbnail);
+                binding.selectedFileThumbnail.setImageBitmap(simplyLoadBitmap(selectedFileUri, activity, width, height));
             } catch (Exception e) {
                 loadFallBackThumbnail(selectedFile, selectedFileUri, binding, activity);
             }
         }
     }
 
+    public static Bitmap simplyLoadBitmap(Uri selectedFileUri, Activity activity, int width, int height) throws IOException {
+        Size mSize = new Size(width, height);
+        CancellationSignal ca = new CancellationSignal();
+        return activity.getContentResolver().loadThumbnail(selectedFileUri, mSize, ca);
+    }
+
     private static void loadFallBackThumbnail(String selectedFile, Uri selectedFileUri, FragmentSendBinding binding, Activity activity) {
 
-        if (selectedFile.endsWith("mp3") || selectedFile.endsWith("wav") || selectedFile.endsWith("ooc")) {
+        if (selectedFile.endsWith("mp3") || selectedFile.endsWith("wav") || selectedFile.endsWith("aiff")) {
             binding.selectedFileThumbnail.setImageResource(R.drawable.headphones_24px);
 
         } else if (selectedFile.endsWith("pdf") || selectedFile.endsWith("txt")) {
             binding.selectedFileThumbnail.setImageResource(android.R.drawable.ic_menu_save);
 
-        }else if (selectedFile.endsWith("mov") || selectedFile.endsWith("mp4") || selectedFile.endsWith("mkv")){
-            loadVideoThumbnailForOlderAPIs(selectedFileUri, binding, activity);
+        } else if (selectedFile.endsWith("mov") || selectedFile.endsWith("mp4") || selectedFile.endsWith("mkv")) {
+            Bitmap videoThumbnail = loadVideoThumbnailForOlderAPIs(selectedFileUri, activity);
+            if (videoThumbnail != null)
+                binding.selectedFileThumbnail.setImageBitmap(videoThumbnail);
 
-        }else if (selectedFile.endsWith("apk")){
+        } else if (selectedFile.endsWith("apk")) {
             binding.selectedFileThumbnail.setImageResource(android.R.drawable.sym_def_app_icon);
-        }else
+        } else
             binding.selectedFileThumbnail.setImageResource(R.drawable.baseline_broken_image);
 
     }
 
-    private static void loadVideoThumbnailForOlderAPIs(Uri selectedFileUri, FragmentSendBinding binding, Activity activity) {
+    public static Bitmap loadVideoThumbnailForOlderAPIs(Uri selectedFileUri, Activity activity) {
         try (MediaMetadataRetriever retriever = new MediaMetadataRetriever()) {
             retriever.setDataSource(activity, selectedFileUri);
-            binding.selectedFileThumbnail.setImageBitmap(retriever.getFrameAtTime());
+            return retriever.getFrameAtTime();
         } catch (Exception e) {
             Log.d("Video Thumbnail Loader", Objects.requireNonNull(e.getMessage()));
         }
+        return null;
     }
 
 }
