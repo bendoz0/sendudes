8e3da0c97c199d04f5c9eb8f080478b4d4ad229a
rohit <verma.rohit.1203@gmail.com>
2024-12-13 23:45:29 +0100

    Reused thumbnail loader methods

--- app/src/main/java/it/startup/sendudes/utils/Db/FilesCursorAdapter.java
+++ app/src/main/java/it/startup/sendudes/utils/Db/FilesCursorAdapter.java
@@ -1,14 +1,12 @@
 package it.startup.sendudes.utils.Db;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.CancellationSignal;
 import android.util.Log;
-import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,20 +14,22 @@ import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.File;
 import java.util.Objects;
 
 import it.startup.sendudes.R;
+import it.startup.sendudes.utils.files_utils.FileUtils;
 
 public class FilesCursorAdapter extends CursorAdapter {
     private Activity activity;
-    private static  class ViewHolder{
+
+    private static class ViewHolder {
         public ImageView image;
         public TextView fileName;
         public TextView fileSize;
         public TextView fileTimeStamp;
         public TextView fileSent;
-        public ViewHolder(View view){
+
+        public ViewHolder(View view) {
             image = view.findViewById(R.id.rowitem_image);
             fileName = view.findViewById(R.id.rowitem_name);
             fileSize = view.findViewById(R.id.rowitem_size);
@@ -37,6 +37,7 @@ public class FilesCursorAdapter extends CursorAdapter {
             fileSent = view.findViewById(R.id.rowitem_sent);
         }
     }
+
     public FilesCursorAdapter(Context context, Cursor cursor, Activity activity) {
         super(context, cursor, 0);
         this.activity = activity;
@@ -50,7 +51,7 @@ public class FilesCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ViewHolder holder;
-        if (view.getTag() == null){
+        if (view.getTag() == null) {
             holder = new ViewHolder(view);
             view.setTag(holder);
         } else {
@@ -67,20 +68,39 @@ public class FilesCursorAdapter extends CursorAdapter {
         holder.fileSize.setText(fSize);
         holder.fileTimeStamp.setText(fTimeStamp);
         holder.fileSent.setText(fSent.equals("1") ? "File Sent" : "File Received");
-        loadSelectedFileThumbnail(Uri.parse(uri), holder);
+        try {
+            loadSelectedFileThumbnail(Uri.parse(uri), holder);
+        }catch (Exception e){
+            String errorMessage = e.getMessage() != null ? e.getMessage() : "aint workinnnnnnnn";
+            Log.d("Error Loading Thumbnail", errorMessage);
+        }
     }
 
     private void loadSelectedFileThumbnail(Uri uri, ViewHolder holder) {
         try {
-            Size mSize = new Size(105, 105);
-            CancellationSignal ca = new CancellationSignal();
-            Bitmap bitmapThumbnail = activity.getContentResolver().loadThumbnail(uri, mSize, ca);
-            System.out.println("THUMBNAIL: " + bitmapThumbnail);
-            holder.image.setImageBitmap(bitmapThumbnail);
-
+            holder.image.setImageBitmap(FileUtils.simplyLoadBitmap(uri, activity, 105, 105));
         } catch (Exception e) {
-            System.out.println("ERROR CREATING THUMBNAIL: " + e.getMessage());
-            holder.image.setImageResource(R.drawable.baseline_broken_image);
+            Log.d("Error Loading Thumbnail", Objects.requireNonNull(e.getMessage()));
+
+            String fileName = String.valueOf(holder.fileName);
+            Log.d("File name", fileName);
+
+            if (fileName.endsWith("mp3") || fileName.endsWith("wav") || fileName.endsWith("aiff")) {
+                holder.image.setImageResource(R.drawable.headphones_24px);
+
+            } else if (fileName.endsWith("pdf") || fileName.endsWith("txt")) {
+                holder.image.setImageResource(android.R.drawable.ic_menu_save);
+
+            } else if (fileName.endsWith("mov") || fileName.endsWith("mp4") || fileName.endsWith("mkv")) {
+                Bitmap videoThumbnail = FileUtils.loadVideoThumbnailForOlderAPIs(uri, activity);
+                if (videoThumbnail != null)
+                    holder.image.setImageBitmap(videoThumbnail);
+
+            } else if (fileName.endsWith("apk")) {
+                holder.image.setImageResource(android.R.drawable.sym_def_app_icon);
+            } else
+                holder.image.setImageResource(R.drawable.baseline_broken_image);
+
         }
     }
 
